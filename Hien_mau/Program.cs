d596bfb39a34050a00e6cc47a8aa4e4c228ff33c
DucQuang0565 <ducquang0565@gmail.com>
2025-05-15 11:34:28 +0700

    setup san api cho project

---
+++ Hien_mau/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// Truy cập vào trong DI container đăng ký DB
+// và cấu hình DB để lấy chuỗi kết nối DefaultConnection trong appsetting.json
+builder.Services.AddDbContext<DatabaseName>
+    (Options => Options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
