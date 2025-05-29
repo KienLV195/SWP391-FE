4b3d33bfcda36f88e1fec8c94c09670d944d1ee3
meelamila <minminkookie2005@gmail.com>
2025-05-29 21:05:00 +0700

    feat: enhance Footer component with navigation links and contact information, update styles for improved layout and readability

--- fe/src/components/common/Footer.jsx
+++ fe/src/components/common/Footer.jsx
@@ -1,17 +1,70 @@
 import "../../styles/components/Footer.scss";
+import { Link } from "react-router-dom";
+import { IoIosArrowForward } from "react-icons/io"; // Icon mũi tên cho danh mục
+import { FaMapMarkerAlt, FaPhone } from "react-icons/fa"; // Icon địa chỉ và điện thoại
+import { MdEmail } from "react-icons/md"; // Icon email
+
+// Hàm cuộn lên đầu trang
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+
+// Hàm cuộn đến phần "Giới thiệu bệnh viện"
+const scrollToHospitalInfo = () => {
+  const hospitalInfoSection = document.querySelector(".hospital-info-section");
+  if (hospitalInfoSection) {
+    hospitalInfoSection.scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+};
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-brand">Axiscare</div>
-      <div className="footer-content">
-        © 2025 Axiscare. All rights reserved.
+      <div className="footer-column">
+        <div className="footer-brand">Axiscare</div>
       </div>
-      <div className="footer-content">
-        Contact:{" "}
-        <a href="mailto:contact@axiscare.com" className="footer-link">
-          contact@axiscare.com
-        </a>
+      <div className="footer-column">
+        <div className="footer-title">Danh mục</div>
+        <div className="footer-item">
+          <IoIosArrowForward className="footer-icon" />
+          <button className="footer-link" onClick={scrollToTop}>
+            Trang chủ
+          </button>
+        </div>
+        <div className="footer-item">
+          <IoIosArrowForward className="footer-icon" />
+          <button className="footer-link" onClick={scrollToHospitalInfo}>
+            Về chúng tôi
+          </button>
+        </div>
+        <div className="footer-item">
+          <IoIosArrowForward className="footer-icon" />
+          <Link to="/donation-guide" className="footer-link">
+            Hướng dẫn hiến máu
+          </Link>
+        </div>
+      </div>
+      <div className="footer-column">
+        <div className="footer-title">Liên hệ với chúng tôi</div>
+        <div className="footer-item">
+          <FaMapMarkerAlt className="footer-icon" />
+          TP HCM - Viet Nam
+        </div>
+        <div className="footer-item">
+          <MdEmail className="footer-icon" />
+          <a href="mailto:vienhhtmtu@nihbt.org.vn" className="footer-link">
+            vienhhtmtu@nihbt.org.vn
+          </a>
+        </div>
+        <div className="footer-item">
+          <FaPhone className="footer-icon" />
+          (028) 3957 1342
+        </div>
       </div>
     </footer>
   );
